a51bd8f9aa1983db97f02577a56c1546b5f3720a
emailradhikako-dotcom <emailradhikako@gmail.com>
2025-10-03 05:54:03 +0545

    Add custom acf-config.php for infonepalonline-theme

---
+++ acf-config.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Advanced Custom Fields Configuration
+ * Add this to functions.php or create inc/acf-config.php
+ */
+if (function_exists('acf_add_local_field_group')) {
+    // ... full content from your config above ...
+} 
+?>
